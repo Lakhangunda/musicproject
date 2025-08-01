c33c5bcbde71999bea0475df03da16d5edbb877c
Lakhan Gunda <134035982+Lakhangunda@users.noreply.github.com>
2025-08-01 11:57:25 +0530

    Delete musicapp directory

--- musicapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import music_list
-
-urlpatterns = [
-    path('', music_list, name='music_list'),
-]
--- musicapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Music
-
-def music_list(request):
-    musics = Music.objects.all()
-    return render(request, 'music_list.html', {'musics': musics})
